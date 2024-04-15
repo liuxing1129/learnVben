d3d23614798cad220f7fe2f43db6cba2164349e7
liuxing <liuxing1129@foxmail.com>
2024-04-15 16:03:27 +0800

    [feat]添加路由

---
+++ src/views/checkDocs/chapter1/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div> 第一章 </div>
+  <v-md-preview :text="text" />
+</template>
+
+<script setup lang="ts">
+  import { ref } from 'vue';
+
+  const text = ref('');
+</script>
+
+<style scoped></style>
---
+++ src/views/checkDocs/chapter2/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div> 第二章 </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
